9dceb45a75f23b089afaf8d7aff59b7ca0cdce48
harshitachauhan9724 <harshitachauhan9724@gmail.com>
2024-08-30 22:56:50 +0530

    Added some more programs

--- loops_cases.py
+++ loops_cases.py
@@ -21,3 +21,52 @@ print(min(number))
 print(max(number))
 print(sum(number))
 
+num = list(range(1,21,2))
+for value in num:
+    print(value)
+
+multiples = list(range(3,31,3))
+for value in multiples:
+    print(value)
+
+cube = []
+for value in range(1,11):
+    cubes = value**3
+    cube.append(cubes)
+print(cube) 
+
+cube = [value**3 for value in range(1,11)]
+print(cube)
+
+flower = ['rose' , 'lily' , 'tulip' , 'gypsy' , 'sunflower']
+print("The first three flowers in the list are: ")
+for flowers in flower[:3]:
+   print(flowers)
+print("\nThe middle three flowers are: ")
+print(flower[1:4])
+print("\nThe last three flowers are: ")
+print(flower[2:])
+
+my_pizza = ['cheese burst' , 'sweet corn' , 'farmhouse']
+friend_pizza = my_pizza[:]
+my_pizza.append('supreme veg')
+friend_pizza.append('cheese pepper')
+print("My favourite pizzas are: ")
+for pizzas in my_pizza[:]:
+    print(pizzas)
+print("My friend's favourite pizzas are: ")    
+for pizzas in friend_pizza[:]:
+    print(pizzas)
+
+buffet = ('idli' , 'dosa' , 'dal' , 'rice' , 'poha')
+for food in buffet:
+    print(food)
+#buffet[0] = 'curry'    
+buffet = ('idli' , 'dosa' , 'dal' , 'rice' , 'poha')
+print("Initial menu: ")
+for items in buffet:
+    print(items)
+buffet = ('idli' , 'dosa' , 'dal' , 'curry' , 'upma')
+print("\nRevised menu: ")
+for items in buffet:
+    print(items)
--- magicians.py
+++ magicians.py
@@ -33,3 +33,41 @@ print(sum(digits))
 
 square = [value**2 for value in range(1,11)]
 print(square)
+
+players = ['ronaldo' , 'kohli' , 'hardik' , 'rohit' , 'yuvraj']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+
+print("Here are the first three players on my team: ")
+for player in players[:3]:
+    print(player.title())
+
+my_foods = ['cake' , 'kulfi' , 'pav bhaji']    
+friend_foods = my_foods[:]
+my_foods.append('ice cream')
+friend_foods.append('momos')
+print("My favourite foods are: ")
+print(my_foods)
+print("\nMy friend's favourite foods are: ")
+print(friend_foods)
+
+dimension = (200 , 50)
+print(dimension[0])
+print(dimension[1])
+
+#dimension[0] = 250
+
+for dimensions in dimension:
+    print(dimensions)
+
+dimensions = (200, 50)
+print("Original dimensions are: ")    
+for dim in dimensions:
+    print(dim)
+dimensions = (400, 100)
+print("\nModified dimensions: ")
+for dim in dimensions:
+    print(dim)
