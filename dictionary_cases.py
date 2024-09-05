348e92c75337a064a1804650765140d4fe189ed5
harshitachauhan9724 <harshitachauhan9724@gmail.com>
2024-09-06 00:18:45 +0530

    Dictionary

---
+++ dictionary_cases.py
@@ -0,0 +1,27 @@
+person = {'first name': 'Eric' , 'last name': 'Mandela' , 'age': 50 , 'city': 'New York'}
+print(person)
+
+numbers = {
+    'ash': 4,
+    'ady': 6,
+    'eric': 1,
+    'ellie': 7,
+    'nova': 2,
+}
+print(numbers)
+
+glossary = {
+    'variable': 'varia',
+    'string': 'stri',
+    'constant': 'const',
+    'python': 'py',
+}
+word = 'variable'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'string'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'constant'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'python'
+print("\n" + word.title() + ": " + glossary[word])
+
