64338ee0b826406ccccf67eb088556829856322d
harshitachauhan9724 <harshitachauhan9724@gmail.com>
2024-09-06 14:36:33 +0530

    Dictionary Programs

--- dictionary.py
+++ dictionary.py
@@ -49,3 +49,119 @@ print("David's favourite language is " +
        language['david'].title() + 
         ".")
 
+user = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+for key, value in user.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+language = {
+    'jen': 'python',
+    'david': 'c++',
+    'ash': 'java',
+    'edward': 'c',
+    }
+for name, languages in language.items():
+    print(name.title() + "'s favourite language is " + languages.title() + ".")
+
+for name in language.keys():
+    print(name.title())
+
+language = {
+    'jen': 'python',
+    'david': 'c++',
+    'ash': 'java',
+    'edward': 'c',
+    }
+friends = ['jen', 'edward']
+for name in language.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() + ", I see your favorite language is " + language[name].title() + "!")
+
+if 'erin' not in language.keys():
+    print("Erin, Please take our poll.")
+
+for name in sorted(language.keys()):
+    print(name.title() + ", Thankyou for taking our poll.")
+
+print("The following languages have been mentioned:")
+for name in language.values():
+    print(name.title())
+
+language = {
+    'jen': 'python',
+    'david': 'c++',
+    'ash': 'java',
+    'edward': 'c++',
+    }
+print("The following languages have been mentioned:")
+for name in set(language.values()):
+    print(name.title())
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+aliens = []
+for alien_num in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+print("Total number of aliens: " + str(len(aliens)))
+
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+print("You ordered a " + pizza['crust'] + "-crust pizza with the following pizza toppings:")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+languages = {
+    'edward': ['c', 'python'],
+    'ash': ['c'],
+    'eric': ['c++', 'java'],
+    'david': ['python', 'php'],
+}
+for name, language in languages.items():
+    print("\n" + name.title() + "'s favourite languages are:")
+    for lang in language:
+        print("\t" + lang.title())
+
+users = {
+    'aeinstein' : {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie' : {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    }
+}
+for username, info in users.items():
+    print("\nUsername: " + username)
+    full_name = info['first'] + " " + info['last']
+    location = info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
+
--- dictionary_cases.py
+++ dictionary_cases.py
@@ -25,3 +25,96 @@ print("\n" + word.title() + ": " + glossary[word])
 word = 'python'
 print("\n" + word.title() + ": " + glossary[word])
 
+glossary = {
+    'variable': 'varia',
+    'string': 'stri',
+    'constant': 'const',
+    'python': 'py',
+    'loop': 'for',
+    'condition': 'if',
+    'otherwise': 'else',
+}
+for key, value in glossary.items():
+    print("\nTerm: " + key.title())
+    print("Meaning: " + value.title())
+
+river = {
+    'nile': 'egypt',
+    'ganga': 'india',
+    'amazon': 'america',
+}
+for water, place in river.items():
+    print("The " + water.title() + " runs through " + place.title() + ".")
+for water in river.keys():
+    print(water.title())
+for place in river.values():
+    print(place.title())
+
+language = {
+    'jen': 'python',
+    'david': 'c++',
+    'ash': 'java',
+    'edward': 'c++',
+    }
+for name, lang in language.items():
+    print(name.title() + ": " + lang.title())
+people = ['jen', 'phil', 'ash', 'sarah']
+for names in people:
+    if names in language.keys():
+        print("Thankyou " + names.title() + ", for taking the poll.")
+    else:
+        print("Please " + names.title() + ", take the poll.")
+
+person_0 = {'name': 'eric', 'profession': 'writer', 'age': 50}
+person_1 = {'name': 'elsa', 'profession': 'engineer', 'age': 28}
+person_2 = {'name': 'ashley', 'profession': 'artist', 'age': 40}
+person = [person_0, person_1, person_2]
+for people in person:
+    print(people)
+
+bholi = {'type': 'cow', 'owner': 'ram'}
+bruno = {'type': 'dog', 'owner': 'david'}
+sona = {'type': 'deer', 'owner': 'shyam'}
+pet = [bholi, bruno, sona]
+for animal in pet:
+    print(animal)
+
+places = {'delhi': 'richa', 'bombay': 'kru', 'chennai': 'ruhi'}
+print("The favourite places of people are:")
+for name, place in places.items():
+    print(name.title() + ":" + place.title())
+
+number = {
+    'dev': [2, 4],
+    'ady': [6],
+    'noa': [1, 8],
+    'mani': [3, 7],
+}
+for name, fav_num in number.items():
+    print("\n" + name.title() + "'s favourite numbers are:")
+    for fav in fav_num:
+        print(fav)
+
+cities = {
+    'kathmandu' : {
+        'country': 'nepal',
+        'population': 1003285,
+        'fact': 'mountains',
+    },
+    'bengaluru' : {
+        'country': 'india',
+        'population': 14008000,
+        'fact': 'advanced',
+    },
+    'mumbai' : {
+        'country': 'india',
+        'population': 21673000,
+        'fact': 'most populated',
+    },
+}
+for name, info in cities.items():
+    print("\nName:" + name.title())
+    population = info['population']
+    fact = info['fact']
+    print("\tPopulation:" + str(population))
+    print("\tFact:" + fact)
