e65f894484a0dc641a1920940627e0cf50554dde
harshitachauhan9724 <harshitachauhan9724@gmail.com>
2024-09-06 17:22:22 +0530

    Adding two new files

---
+++ input_while_cases.py
@@ -0,0 +1,17 @@
+# car = input("What kind of car would you like?")
+# print("Let me find you a " + car + ".")
+
+# restaurant = input("How many people are there in your dinner group?")
+# restaurant = int(restaurant)
+# if restaurant >= 8:
+#    print("You will have to wait for a table.")
+# else:
+#    print("Your table is ready.")
+
+# number = input("Enter a number.")
+# number = int(number)
+# if number % 10 == 0:
+#    print(str(number) + " is a multiple of 10.")
+# else:
+#    print(str(number) + " is not a multiple of 10.")
+
---
+++ input_while_loop.py
@@ -0,0 +1,48 @@
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+# name = input("Please tell me your name: ")
+# print("Hello " + name + "!")
+
+# prompt = "If you tell us who you are, we can personalize the message you see."
+# prompt += "\nWhat is you name?"
+# name = input(prompt)
+# print("\nHello " + name + "!")
+
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 36:
+#    print("\nYou are tall enough to ride!")
+# else:
+#    print("\nYou will be able to ride when you are a little older!")
+
+# number = input("Enter a number, and i will tell you if it's even or odd!")
+# number = int(number)
+# if number % 2 == 0:
+#   print("\nThe number " + str(number) + " is even.")
+# else:
+#    print("\nThe number " + str(number) + " is")
+
+# current_num = 1
+# while current_num <= 5:
+#     print(current_num)
+#     current_num += 1
+
+# prompt = "\nTell me something, and I will repeat it back to you."
+# prompt += "\nEnter 'quit' to end the program."
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != 'quit':
+#       print(message)
+
+# prompt = "\nTell me something, and I will repeat it back to you."
+# prompt += "\nEnter 'quit' to end the program."
+# active = True
+# while active:
+#     message = input(prompt)
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+
